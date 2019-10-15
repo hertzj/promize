eb2e29002853b04492cf6ffe305103cea99c0975
Jake Hertz <hertzjake@gmail.com>
2019-10-14 21:45:17 -0400

    kind of worked

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,35 @@
-const syncCallbacker = null;
+const syncCallbacker = (function1, function2) => {
+    if (typeof function1 !== 'function' || typeof function2 !== 'function') {
+        throw 'not a function!';
+    }
 
-const asyncCallbacker = null;
+    const val = function1()
+    return function2(val)
+};
+
+const asyncCallbacker = (func1, func2) => {
+    if (typeof func1 !== 'function' || typeof func2 !== 'function') {
+        throw 'not a function!';
+    }
+
+    const data = 100
+    const done = (num) => {
+        return func2(num, done)
+    }
+    
+    func1(data, done)    
+};
 
 module.exports = { syncCallbacker, asyncCallbacker };
+
+
+// const syncCallbacker = (...args) => {
+//     args.forEach(arg => {
+//         if (typeof arg !== 'function') {
+//             throw 'not a function'
+//         }
+//     })
+
+
+    
+// };
--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,12 @@
-const basicPromise = null;
-const funcPromise = () => null;
-const chainedPromise = prom => null;
+const basicPromise = new Promise((resolve, reject) => {
+    resolve('basicPromise')
+})
+const funcPromise = () => new Promise((resolve, reject) => {
+    resolve('funcPromise')
+});
+const chainedPromise = prom => new Promise((resolve, reject) => {
+    resolve();
+}).then(prom);
 const rejectedPromise = () => null;
 
 module.exports = { basicPromise, chainedPromise, rejectedPromise, funcPromise };
